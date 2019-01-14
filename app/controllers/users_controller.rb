35a87be82d8ac7c5390b7a8492d3bf2f3e7a3f78
Kowa Jia Liang <jlkowa1229@gmail.com>
2019-01-14 22:27:48 +0800

    Log in done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params) # Not the final implementation!
     if @user.save
       # Handle a successful save.
-      flash[:success] = "Welcome to Freelancer apps!"
+      log_in @user
+      flash[:success] = "Welcome to Freelancer apps, #{@user.fullname}!"
       redirect_to @user
     else
       render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  get '/signup',   to: 'users#new'
   root 'public#home'
 
   # get 'public/home'
