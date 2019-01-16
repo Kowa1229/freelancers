f576baf1c7872ade686ccf9c3fb0c75826c63da3
Kowa Jia Liang <jlkowa1229@gmail.com>
2019-01-17 00:36:45 +0800

    heroku email test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,16 +67,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :smtp
   host = 'freelancersapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['app122029647@heroku.com'],
-      :password       => ENV['jghzbans2688'],
+      :user_name      => 'app122029647@heroku.com',
+      :password       => 'jghzbans2688',
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
