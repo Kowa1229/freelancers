88b91f6a23e2534ead6a9927496697c0b36546d5
Kowa Jia Liang <jlkowa1229@gmail.com>
2019-01-17 12:43:27 +0800

    Heroku email test 5

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
-ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['app122029647@heroku.com'],
-    :password => ENV['jghzbans2688'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
+# ActionMailer::Base.smtp_settings = {
+#     :user_name => ENV['app122029647@heroku.com'],
+#     :password => ENV['jghzbans2688'],
+#     :domain => 'heroku.com',
+#     :address => 'smtp.sendgrid.net',
+#     :port => 587,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+# }
 
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,15 +71,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'freelancersapp.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #     :address        => 'smtp.sendgrid.net',
-  #     :port           => '587',
-  #     :authentication => :plain,
-  #     :user_name      => ENV['app122029647@heroku.com'],
-  #     :password       => ENV['jghzbans2688'],
-  #     :domain         => 'heroku.com',
-  #     :enable_starttls_auto => true
-  # }
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['app122029647@heroku.com'],
+      :password       => ENV['jghzbans2688'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
