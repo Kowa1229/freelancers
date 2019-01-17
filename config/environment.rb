51cee3e7eeee45bbefa325a2e0a232b60586a47f
Kowa Jia Liang <jlkowa1229@gmail.com>
2019-01-17 12:25:59 +0800

    Heroku email test 3

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Initialize the Rails application.
-Rails.application.initialize!
-
 ActionMailer::Base.smtp_settings = {
-    :user_name => 'app122029647@heroku.com',
-    :password => 'jghzbans2688',
+    :user_name => ENV['app122029647@heroku.com'],
+    :password => ENV['jghzbans2688'],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
 }
+
+# Initialize the Rails application.
+Rails.application.initialize!
